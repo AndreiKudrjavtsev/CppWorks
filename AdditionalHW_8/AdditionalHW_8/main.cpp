8557c54a564e3aa7da879bc3752113a27831e723
Andrey Kudryavtsev <tmac1attack@gmail.com>
2017-04-27 20:14:08 +0300

    additional hw 8

---
+++ AdditionalHW_8/AdditionalHW_8/main.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <map>
+#include <Windows.h>
+
+using namespace std;
+
+void commonBook() {
+	auto people = map<string, string>();
+	string name;
+	string book;
+	while (true) {
+		cin >> name >> book;
+		auto ins = people.insert(pair<string, string>(book, name));
+		if (ins.second == false) {
+			cout << "Интересы совпадают: " << ins.first->second
+				<< " " << name << endl;
+			return;
+		}
+	}
+}
+
+void commonInterests() {
+	auto people = map<pair<string, string>, string>();
+	string name, book, band;
+	while (true) {
+		cin >> name >> book >> band;
+		auto interests = pair<string, string>(book, band);
+		auto ins = people.insert(pair<pair<string, string>, string>(interests, name));
+		if (ins.second == false) {
+			cout << "Интересы совпадают: " << ins.first->second
+				<< " " << name << endl;
+			return;
+		}
+	}
+}
+
+void commonInterest() {
+	auto peopleBook = map<string, string>();
+	auto peopleBand = map<string, string>();
+	string name, book, band;
+	while (true) {
+		cin >> name >> book >> band;
+		auto insBook = peopleBook.insert(pair<string, string>(book, name));
+		auto insBand = peopleBand.insert(pair<string, string>(band, name));
+		if (insBook.second == false || insBand.second == false) {
+			cout << "Интересы частично совпадают: " << insBook.first->second
+				<< " " << name << endl;
+			return;
+		}
+	}
+}
+
+int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	// 1st task
+	commonBook();
+	// 2nd task
+	commonInterests();
+	// 3rd task
+	commonInterest();
+
+	return 0;
+}
